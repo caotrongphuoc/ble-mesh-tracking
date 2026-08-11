c231a78642d0c6f708cbc5f61d9ef2138cdbc496
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-08-11 20:58:22 +0700

    [REFACTOR] extract bmt_factory_reset to shared components/

--- apps/relay/components/bmt_factory_reset/bmt_factory_reset.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/* Start a background task that watches the BOOT button (GPIO0). Holding it
- * continuously for 10 seconds triggers a FACTORY RESET (erases the whole
- * NVS: node table, NetKey/AppKey, all other config; firmware is NOT
- * touched) and then reboots.
- * Call once at boot, as early as possible. */
-void bmt_factory_reset_init(void);
